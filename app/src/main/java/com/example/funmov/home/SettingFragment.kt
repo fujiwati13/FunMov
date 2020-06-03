8183b213c933fb37328980c9283fbceeba7d80d3
Fujiwati Himatul <fujiwati.himatul@gmail.com>
2020-06-03 08:25:17 +0700

    add code

--- app/src/main/java/com/example/funmov/home/SettingFragment.kt
+++ app/src/main/java/com/example/funmov/home/SettingFragment.kt
@@ -5,13 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
 import com.example.funmov.R
+import com.example.funmov.utils.Preferences
+import kotlinx.android.synthetic.main.fragment_setting.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class SettingFragment : Fragment() {
+
+    lateinit var preferences: Preferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +26,17 @@ class SettingFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
     }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
 
+        preferences = Preferences(context!!.applicationContext)
+
+        iv_nama.text = preferences.getValues("nama")
+        tv_email.text = preferences.getValues("email")
+
+        Glide.with(this)
+            .load(preferences.getValues("url"))
+            .apply(RequestOptions.circleCropTransform())
+            .into(iv_profile)
+    }
 }
